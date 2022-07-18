4083bbb131d7b59559a6946cf9a181ca4dca5d4e
fender <mohammad.fendereski@gmail.com>
2022-07-19 02:50:34 +0430

    first scrapers

--- app.py
+++ app.py
@@ -1,1 +1,36 @@
-print('hi')
+import mechanicalsoup
+
+browser = mechanicalsoup.Browser()
+url = "http://olympus.realpython.org/login"
+login_page = browser.get(url)
+login_html = login_page.soup
+
+form = login_html.select("form")[0]
+form.select("input")[0]["value"] = "zeus"
+form.select("input")[1]["value"] = "ThunderDude"
+
+profiles_page = browser.submit(form, login_page.url)
+
+links = profiles_page.soup.select("a")
+
+base_url = "http://olympus.realpython.org"
+for link in links:
+    address = base_url + link["href"]
+    text = link.text
+    print(f"{text}: {address}")
+
+#Repeatedly request data from a website to check for updates
+import time
+
+browser = mechanicalsoup.Browser()
+
+for i in range(4):
+    page = browser.get("http://olympus.realpython.org/dice")
+    tag = page.soup.select("#result")[0]
+    result = tag.text
+    print(f"The result of your dice roll is: {result}")
+
+    # Wait 10 seconds if this isn't the last request 
+    # because we don't wanna wait an extra 10 sec after the last request is done
+    if i < 3:
+        time.sleep(10)
